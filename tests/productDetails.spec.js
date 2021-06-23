6eea29544e531f30e9601000119abcf5ada57a80
Marcus Vinicius Mendes do Nascimento <marcus.mendes89@gmail.com>
2021-06-22 21:51:49 -0300

    Requisito productDetails funcionando

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,34 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(
+      Array.isArray(productDetails('sabao', 'detergente')),
+      true
+    );
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails('sabao', 'detergente').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(
+      typeof productDetails('sabao', 'detergente')[0],
+      'object'
+    );
+    assert.strictEqual(
+      typeof productDetails('sabao', 'detergente')[1],
+      'object'
+    );
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(
+      productDetails('sabao', 'detergente')[0],
+      productDetails('sabao', 'detergente')[1]
+    );
     // Teste que os dois productIds terminam com 123.
+    assert.strictEqual(
+      productDetails('sabao', 'detergente')[0].details.productId.slice(-3),
+      productDetails('sabao', 'detergente')[1].details.productId.slice(-3)
+    );
   });
 });
+//Metodo slice() para extração da string.
+//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/slice
